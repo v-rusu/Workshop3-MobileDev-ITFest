63de62b98f7bb0c6698d602bb5b7cbfc4e1f3ea4
Vlad Rusu <v@vladr.com>
2016-04-13 21:04:37 +0300

    Calculator Project

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
+enum Operation {
+    case Plus
+    case Minus
+    case Divide
+    case Multiply
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet var digitsButtons: [UIButton]!
+    
+    var writtenNumber:Int = 0 {
+        didSet {
+            label.text = String(writtenNumber)
+        }
+    }
+    
+    var resultingNumber:Int = 0 {
+        didSet {
+            label.text = String(resultingNumber)
+        }
+    }
+    
+    var operation:Operation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func plusButtonPressed(sender: AnyObject) {
+        operation = Operation.Plus
+        
+        resultingNumber = writtenNumber
+        writtenNumber = 0
     }
-
+    
+    @IBAction func minusButtonPressed(sender: AnyObject) {
+        operation = Operation.Minus
+        
+        resultingNumber = writtenNumber
+        writtenNumber = 0
+    }
+    
+    @IBAction func multiplyButtonPressed(sender: UIButton) {
+        operation = Operation.Multiply
+        
+        resultingNumber = writtenNumber
+        writtenNumber = 0
+    }
+    
+    @IBAction func divideButtonPressed(sender: AnyObject) {
+        operation = Operation.Divide
+        
+        resultingNumber = writtenNumber
+        writtenNumber = 0
+    }
+    
+    @IBAction func equalsButtonPressed(sender: AnyObject) {
+        switch operation! {
+            case .Plus: resultingNumber += writtenNumber
+            case .Minus: resultingNumber -= writtenNumber
+            case .Multiply: resultingNumber *= writtenNumber
+            case .Divide: resultingNumber /= writtenNumber
+        }
+        
+        writtenNumber = resultingNumber
+    }
+    
+    @IBAction func digitButtonPressed(sender: UIButton) {
+        let index = digitsButtons.indexOf(sender)
+        
+        writtenNumber = writtenNumber * 10 + index!.advancedBy(0)
+    }
+    
+    @IBAction func clearButtonPressed(sender: AnyObject) {
+        writtenNumber = 0
+        resultingNumber = 0
+    }
+    
+    @IBAction func bkspButtonPressed(sender: AnyObject) {
+        writtenNumber /= 10
+    }
 
 }
 
